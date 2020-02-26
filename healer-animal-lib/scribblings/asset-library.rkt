b18272a94391dacb5ebd7aab1672859b9451f58b
Sara <sara@thoughtstem.com>
2020-02-25 16:55:05 -0800

    fixing healer-animal docs to mc-languages pages

--- healer-animal-lib/scribblings/asset-library.rkt
+++ healer-animal-lib/scribblings/asset-library.rkt
@@ -4,6 +4,9 @@
           define-assets-from)
 
 
-@(title "Assets")
+@(title "Sprites")
+
+Note: Not all sprites are available in Ratchet -- only the sprites listed in the legend panel can be accessed in that language in Ratchet.
+
 @(require (submod animal-assets/assets asset-docs))
 @(doc-all (submod animal-assets/assets asset-docs))
